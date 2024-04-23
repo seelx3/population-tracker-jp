b88503182ec3fe9b0971ed498fff6013bfcb1749
seelx3 <seelx3@gmail.com>
2024-04-24 01:36:17 +0900

    fix: add TODO

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import PopTrackLogo from "@/app/ui/pop-track-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { lusitana } from "@/app/ui/fonts";
 
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+
+const blue = "rgb(59 130 246)";
+const gray = "rgb(249 250 251)";
 
 const styles = {
   main: css({
@@ -19,48 +19,24 @@ const styles = {
     display: "flex",
     flexGrow: 0,
     height: "4rem",
-    alignItems: "flex-end",
+    alignItems: "flex-center",
     borderRadius: "0.5rem",
-    backgroundColor: "rgb(59 130 246)",
+    backgroundColor: blue,
     padding: "1rem",
   }),
-  contentContainer: css({
-    marginTop: "1rem",
-    display: "flex",
-    flexGrow: 1,
-    flexDirection: "column",
-    gap: "1rem",
-    "@media (min-width: 768px)": {
-      flexDirection: "row",
-    },
-  }),
-  leftContent: css({
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    gap: "1.5rem",
+  checkbox: css({
     borderRadius: "0.5rem",
-    backgroundColor: "#f9fafb",
-    padding: "1.5rem",
-    width: "100%",
-    "@media (min-width: 768px)": {
-      width: "40%",
-      padding: "3rem",
-    },
+    backgroundColor: gray,
+    height: "4rem",
+    marginTop: "1rem",
+    padding: "1rem",
   }),
-  rightContent: css({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "1.5rem",
-    width: "100%",
-    "@media (min-width: 768px)": {
-      width: "60%",
-      paddingLeft: "7rem",
-      paddingRight: "7rem",
-      paddingTop: "3rem",
-      paddingBottom: "3rem",
-    },
+  charPane: css({
+    borderRadius: "0.5rem",
+    backgroundColor: gray,
+    height: "12rem",
+    marginTop: "1rem",
+    padding: "1rem",
   }),
 };
 
@@ -70,71 +46,11 @@ export default function Page() {
       <div css={styles.header}>
         <PopTrackLogo />
       </div>
-      <div css={styles.contentContainer}>
-        <div css={styles.leftContent}>
-          <p
-            className={`${lusitana.className}`}
-            css={css({
-              margin: 0,
-              fontSize: "1.25rem",
-              lineHeight: "1.75rem",
-              color: "rgb(31 41 55)",
-              "@media (min-width: 768px)": {
-                fontSize: "1.875rem",
-                lineHeight: "1.5",
-              },
-            })}
-          >
-            <strong>都道府県</strong> 別の人口を表示するよ！{" "}
-            <a
-              href="https://nextjs.org/learn/"
-              css={css({
-                textDecorationLine: "none",
-                color: "rgb(59 130 246)",
-              })}
-            >
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            css={css({
-              display: "flex",
-              alignItems: "center",
-              gap: "1.25rem",
-              alignSelf: "flex-start",
-              padding: "0.75rem 1.5rem",
-              borderRadius: "0.375rem",
-              backgroundColor: "rgb(59 130 246)",
-              color: "white",
-              fontSize: "0.875rem",
-              fontWeight: "500",
-              transition: "background-color 0.2s",
-              textDecorationLine: "none",
-              "&:hover": {
-                backgroundColor: "rgb(49 100 236)",
-              },
-              "@media (min-width: 768px)": {
-                padding: "1rem 2rem",
-                fontSize: "1rem",
-              },
-            })}
-          >
-            <span>Log in</span>
-            <ArrowRightIcon
-              css={css({
-                width: "1.25rem",
-                height: "1.25rem",
-                "@media (min-width: 768px)": {
-                  width: "1.5rem",
-                  height: "1.5rem",
-                },
-              })}
-            />
-          </Link>
-        </div>
-        <div css={styles.rightContent}></div>
+      <div css={styles.checkbox}>
+        <p>TODO: add checkbox</p>
+      </div>
+      <div css={styles.charPane}>
+        <p>TODO: add chart</p>
       </div>
     </main>
   );
