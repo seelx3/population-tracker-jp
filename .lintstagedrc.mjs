79279f53ab14637df2fdccc14829c9ab248c6b33
seelx3 <seelx3@gmail.com>
2024-04-27 03:35:07 +0900

    fix: lintstagedrc

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 export default {
   "./**/*.{cjs,css,html,js,json,md,mjs,mts,ts,tsx,yml}": (files) => {
     const commands = [
-      `node node_modules/.bin/prettier --cache --write ${files.join(" ")}`,
+      `node node_modules/.bin/prettier --cache --write ${files.join(" ")} --plugin=prettier-plugin-organize-imports`,
     ];
     if (files.some((file) => file.endsWith("package.json"))) {
       commands.push("node node_modules/.bin/sort-package-json");
