b653ab8977b7ad76d1f75a6ab9ce45ac3805fd49
seel <61373111+seelx3@users.noreply.github.com>
2024-04-27 01:14:50 +0900

    refactor: css modules (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,20 @@
-"use client";
+import PopTrackLogo from "@/app/ui/popu-track-logo";
 
-import PopTrackLogo from "@/app/ui/pop-track-logo";
-
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
 import { ChartPane } from "./ChartPane";
 import CheckBoxPane from "./CheckBoxPane";
 
-const blue = "rgb(59 130 246)";
-const gray = "rgb(249 250 251)";
+import styles from "./styles.module.css";
 
-const styles = {
-  main: css({
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "100vh",
-    padding: "1.5rem",
-  }),
-  header: css({
-    display: "flex",
-    flexGrow: 0,
-    height: "4rem",
-    alignItems: "flex-center",
-    borderRadius: "0.5rem",
-    backgroundColor: blue,
-    padding: "1rem",
-  }),
-  checkbox: css({
-    borderRadius: "0.5rem",
-    backgroundColor: gray,
-    marginTop: "1rem",
-    padding: "1rem",
-  }),
-  charPane: css({
-    borderRadius: "0.5rem",
-    backgroundColor: gray,
-    height: "12rem",
-    marginTop: "1rem",
-    padding: "1rem",
-  }),
-};
-
-export default function Page() {
+export default async function Page() {
   return (
-    <main css={styles.main}>
-      <div css={styles.header}>
+    <main className={styles.main}>
+      <div className={styles.header}>
         <PopTrackLogo />
       </div>
-      <div css={styles.checkbox}>
+      <div className={styles.checkbox}>
         <CheckBoxPane />
       </div>
-      <div css={styles.charPane}>
+      <div className={styles.charPane}>
         <ChartPane />
       </div>
     </main>
--- app/ui/popu-track-logo.tsx
+++ app/ui/popu-track-logo.tsx
@@ -1,36 +1,26 @@
 import { ChartBarIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className}`}
-      css={css({
+      style={{
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
         gap: "1rem",
         lineHeight: 1,
         color: "white",
-      })}
+      }}
     >
       <ChartBarIcon
-        css={css({
+        style={{
           height: "3rem",
           width: "3rem",
-        })}
+        }}
       />
-      <p
-        css={css({
-          margin: 0,
-          fontSize: "2.75rem",
-        })}
-      >
-        PopuTrack
-      </p>
+      <p style={{ margin: 0, fontSize: "2.75rem" }}>PopuTrack</p>
     </div>
   );
 }
