125b5951111553b80e0609f82bc5ea66a212ed07
seelx3 <seelx3@gmail.com>
2024-05-01 19:29:31 +0900

    test: update e2e test

--- tests/e2e/popuTrack.spec.ts
+++ tests/e2e/popuTrack.spec.ts
@@ -27,7 +27,7 @@ test("東京都のチェックボックスのチェックの切り替え", async
   await expect(legendItem).toHaveCount(1);
   await expect(legendItem).toHaveText("東京都");
 
-  // 東京都のチェックボックスをクリックする
+  // 東京都のチェックボックスのチェックを外す
   await page.uncheck("#東京都");
 
   // Highcharts の折れ線グラフがない
@@ -42,40 +42,74 @@ test("複数の都道府県を選択する", async ({ page }) => {
   await page.check("#北海道");
   await page.check("#群馬県");
 
+  // 折れ線グラフが 2 本描画されている
   await expect(page.locator(".highcharts-tracker-line")).toHaveCount(2);
-  await expect(page.locator(".highcharts-legend-item")).toHaveCount(2);
+
+  // 凡例が 2 つ表示されている
+  const legendItem = page.locator(".highcharts-legend-item");
+  await expect(legendItem).toHaveCount(2);
 
   // 東京都、京都府のチェックボックスをクリックする
   await page.check("#東京都");
   await page.check("#京都府");
 
   await expect(page.locator(".highcharts-tracker-line")).toHaveCount(4);
-  await expect(page.locator(".highcharts-legend-item")).toHaveCount(4);
+  await expect(legendItem).toHaveCount(4);
+
+  // 北海道と京都府のチェックボックスのチェックを外す
+  await page.uncheck("#北海道");
+  await page.uncheck("#京都府");
+
+  // 折れ線グラフが 2 本描画されている
+  await expect(page.locator(".highcharts-tracker-line")).toHaveCount(2);
+
+  // 凡例に群馬県と東京都のみが表示されている
+  await expect(legendItem).toHaveCount(2);
+  await expect(legendItem.nth(0)).toHaveText("群馬県");
+  await expect(legendItem.nth(1)).toHaveText("東京都");
 });
 
 test("人口のタブを切り替える", async ({ page }) => {
-  // 東京都のチェックボックスをクリックする
-  await page.check("#東京都");
+  // 埼玉県のチェックボックスをクリックする
+  await page.check("#埼玉県");
 
   const title = page.locator(".highcharts-title");
+
+  // Highcharts の class が highcharts-point である要素が 19 個ある
+  await expect(page.locator(".highcharts-point")).toHaveCount(19);
 
   // Highcharts のタイトルがデフォルトで "総人口の推移" になっている
   await expect(title).toHaveText("総人口の推移");
 
   // "年少人口" タブをクリックする
   await page.click("text=年少人口");
+
   // Highcharts のタイトルが "年少人口の推移" になっている
   await expect(title).toHaveText("年少人口の推移");
+
+  // 折れ線グラフの要素に 1960 年の埼玉県の年少人口データが含まれている
+  await expect(
+    page.locator('[aria-label="1960, 752,024. 埼玉県."]'),
+  ).toHaveCount(1);
 
   // "生産年齢人口" タブをクリックする
   await page.click("text=生産年齢人口");
   await expect(title).toHaveText("生産年齢人口の推移");
+  await expect(
+    page.locator('[aria-label="1960, 1,545,841. 埼玉県."]'),
+  ).toHaveCount(1);
 
   // "老年人口" タブをクリックする
   await page.click("text=老年人口");
   await expect(title).toHaveText("老年人口の推移");
+  await expect(
+    page.locator('[aria-label="1960, 133,006. 埼玉県."]'),
+  ).toHaveCount(1);
 
   // "総人口" タブをクリックする
   await page.click("text=総人口");
   await expect(title).toHaveText("総人口の推移");
+  await expect(
+    page.locator('[aria-label="1960, 2,430,871. 埼玉県."]'),
+  ).toHaveCount(1);
 });
