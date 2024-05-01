36300a574dea126dc5940411acb36b0383b03f79
seelx3 <seelx3@gmail.com>
2024-05-01 15:19:57 +0900

    refactor: style

--- app/components/organisms/PopuTrackLogo.tsx
+++ app/components/organisms/PopuTrackLogo.tsx
@@ -6,13 +6,8 @@ import { ChartBarIcon } from "@heroicons/react/24/outline";
 export function PopuTrackLogo() {
   return (
     <div className={`${lusitana.className} ${style.popuTrackLogo}`}>
-      <ChartBarIcon
-        style={{
-          height: "1.5rem",
-          width: "1.5rem",
-        }}
-      />
-      <p style={{ fontSize: "1.5rem" }}>都道府県別人口推移</p>
+      <ChartBarIcon className={style.popuTrackLogoIcon} />
+      <p className={style.popuTrackLogoText}>都道府県別人口推移</p>
     </div>
   );
 }
