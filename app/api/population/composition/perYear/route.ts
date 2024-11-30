df3b08d3baaa479fb5fc9f96d2742768f2d0dd39
seelx3 <seelx3@gmail.com>
2024-12-01 06:21:58 +0900

    refactor: query zod validation

--- app/api/population/composition/perYear/route.ts
+++ app/api/population/composition/perYear/route.ts
@@ -1,9 +1,24 @@
+import { z } from "zod";
+
 const POPULATION_COMPOSITION_API_URL =
   "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
+
+const querySchema = z.object({
+  prefCode: z.string().refine((val) => {
+    const intValue = parseInt(val);
+    return intValue >= 1 && intValue <= 47;
+  }),
+});
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const prefCode = searchParams.get("prefCode");
+
+  try {
+    querySchema.parse({ prefCode });
+  } catch {
+    throw new Error("Invalid query parameter");
+  }
 
   return await fetch(
     `${POPULATION_COMPOSITION_API_URL}?cityCode=-&prefCode=${prefCode}`,
