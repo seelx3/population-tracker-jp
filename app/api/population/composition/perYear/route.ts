93d33a91457260cd83f9bdeec384bfb38327a543
seelx3 <seelx3@gmail.com>
2024-12-01 10:36:55 +0900

    fix: use process env

--- app/api/population/composition/perYear/route.ts
+++ app/api/population/composition/perYear/route.ts
@@ -1,8 +1,5 @@
 import { z } from "zod";
 
-const POPULATION_COMPOSITION_API_URL =
-  "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
-
 const querySchema = z.object({
   prefCode: z.string().refine((val) => {
     const intValue = parseInt(val);
@@ -21,7 +18,7 @@ export async function GET(req: Request) {
   }
 
   return await fetch(
-    `${POPULATION_COMPOSITION_API_URL}?cityCode=-&prefCode=${prefCode}`,
+    `${process.env.POPULATION_COMPOSITION_API_URL}?cityCode=-&prefCode=${prefCode}`,
     {
       method: "GET",
       headers: {
--- app/api/prefectures/route.ts
+++ app/api/prefectures/route.ts
@@ -1,8 +1,5 @@
-const PREFECTURES_API_URL =
-  "https://opendata.resas-portal.go.jp/api/v1/prefectures";
-
 export async function GET() {
-  return await fetch(PREFECTURES_API_URL, {
+  return await fetch(`${process.env.PREFECTURES_API_URL}`, {
     method: "GET",
     headers: {
       "X-API-KEY": process.env.RESAS_API_KEY || "",
