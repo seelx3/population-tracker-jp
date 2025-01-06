79305293918d84471a08824c199f8414c1ce14f6
seelx3 <seelx3@gmail.com>
2025-01-07 07:20:55 +0900

    chore: add tanstack query devtools

--- app/components/PopuTrackPane.tsx
+++ app/components/PopuTrackPane.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { ChartPane } from "./ChartPane";
 import { CheckBoxPane } from "./CheckBoxPane";
@@ -12,6 +13,7 @@ export const PopuTrackPane: React.FC = () => {
     <QueryClientProvider client={queryClient}>
       <CheckBoxPane />
       <ChartPane />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
