6fafeb36b1e6f8ea5458b74cab4df797a092666d
seelx3 <seelx3@gmail.com>
2025-01-09 02:14:50 +0900

    chore: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: ["tests/e2e/**/*", "node_modules/**/*"],
+  },
+  resolve: {
+    alias: {
+      "@": __dirname,
+    },
+  },
+});
