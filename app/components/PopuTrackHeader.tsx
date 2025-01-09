31645ebed5c05939efd521b9df26f0ff5c06aa85
seelx3 <seelx3@gmail.com>
2025-01-09 21:34:37 +0900

    feat: add link to github repository

--- app/components/PopuTrackHeader.tsx
+++ app/components/PopuTrackHeader.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import { ChartBarIcon } from "@heroicons/react/24/outline";
+import { HiOutlineChartBar } from "react-icons/hi2";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 import style from "@/app/styles/popuTrackHeader.module.css";
 
 import { lusitana } from "@/app/ui/fonts";
 
+const GITHUB_URL = "https://github.com/seelx3/population-tracker-jp";
+
 export const PopuTrackHeader: React.FC = () => {
   return (
-    <div className={`${lusitana.className} ${style.popuTrackHeader}`}>
-      <ChartBarIcon className={style.popuTrackLogoIcon} />
-      <p className={style.popuTrackLogoText}>都道府県別人口推移</p>
+    <div className={`${lusitana.className} ${style.header}`}>
+      <div className={style.headerLogoWrapper}>
+        <HiOutlineChartBar className={style.headerLogoIcon} />
+        <p className={style.headerTitle}>都道府県別人口推移</p>
+      </div>
+      <div>
+        <Link href={GITHUB_URL}>
+          <div className={style.headerGithubIconContainer}>
+            <FaGithub className={style.headerGithubIcon} />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
