77b7727a12711fe64ec6c5a73541a270f6bb3f7b
seelx3 <seelx3@gmail.com>
2025-01-09 20:36:29 +0900

    chore: add eslint-plugin-import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
 import nextPlugin from "@next/eslint-plugin-next";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config[]} */
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+/** @type {import('eslint').Linter.Config[]} */ // eslint-disable-next-line import/no-anonymous-default-export
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "next", "prettier"],
+  }),
   {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
     plugins: {
       ...nextPlugin.plugins,
+    },
+    rules: {
+      "import/order": "error",
     },
   },
   pluginJs.configs.recommended,
