98fd4ae8087dcbd28374a53db2ad779a25c16749
seelx3 <seelx3@gmail.com>
2025-01-09 20:46:34 +0900

    chore: update .lintstagedrc

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,9 @@
 export default {
   "./**/*.{cjs,css,html,js,json,md,mjs,mts,ts,tsx,yml}": (files) => {
     const commands = [
-      `node node_modules/.bin/prettier --cache --write ${files.join(" ")} --plugin=prettier-plugin-organize-imports`,
+      `node node_modules/.bin/prettier --cache --write ${files.join(" ")}`,
+      `node node_modules/.bin/eslint --cache --fix ${files.join(" ")}`,
+      `node node_modules/.bin/eslint --cache ${files.join(" ")}`,
     ];
     if (files.some((file) => file.endsWith("package.json"))) {
       commands.push("node node_modules/.bin/sort-package-json");
